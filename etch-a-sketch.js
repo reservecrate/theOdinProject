2ef5f28de01383eb85a2e07db3de0481f06b6c53
Aldiyar Kabulov <reservecrate@gmail.com>
2021-06-08 09:36:27 +0600

    Etch-A-Sketch

---
+++ etch-a-sketch.js
@@ -0,0 +1,52 @@
+const headingElement = document.createElement('h1');
+headingElement.textContent = 'Etch-A-Sketch';
+const containerDiv = document.createElement('div');
+const resetButton = document.createElement('button');
+resetButton.textContent = 'Reset';
+document.body.appendChild(headingElement).id = 'main-heading';
+document.body.appendChild(resetButton).id = 'reset-button';
+document.body.appendChild(containerDiv).id = 'container-div';
+const userNumber = parseInt(prompt('Enter the number of cells in a row/column: '));
+if (userNumber > 100 || typeof userNumber !== 'number') {
+    alert('Invalid value entered. Please try again');
+    document.reload();
+}
+
+const createCell = () => {
+    return document.createElement('div');
+};
+
+const createRow = () => {
+    let gridRow = document.createElement('div');
+    for (let i = 0; i < userNumber; i++) {
+        gridRow.appendChild(createCell()).className = 'cell-element';
+    }
+    return gridRow;
+};
+
+const createGrid = () => {
+    for (let i = 0; i < userNumber; i++) {
+        containerDiv.appendChild(createRow()).className = 'grid-row';
+    }
+};
+
+createGrid();
+
+let cells = document.querySelectorAll('.cell-element');
+cells.forEach(cell => {
+    cell.addEventListener('mouseenter', () => {
+        cell.classList.add('touched');
+    });
+    cell.addEventListener('click',() => {
+        cell.classList.toggle('clicked');
+        cell.textContent = 'I got clicked!';
+    })
+    cell.addEventListener('dblclick', () => {
+        cell.classList.toggle('doubleclicked');
+        cell.textContent = 'I got double clicked!';
+    })
+})
+
+resetButton.addEventListener('click', () => {
+    location.reload();
+})
